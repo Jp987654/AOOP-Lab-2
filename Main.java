36878106629ba4a5ce72ad8e29d37c3b32fb7bab
JP <jose1950rivera1950@gmail.com>
2026-09-14 15:45:19 -0600

    Update log + chessboard class+ main user input collection

--- Main.java
+++ Main.java
@@ -1,19 +1,53 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+
 public class Main {
+  //Enums for PieceType,Color, and Location of x
+  enum Color {
+        WHITE, BLACK
+    }
+    enum PieceType {
+        PAWN, KNIGHT, BISHOP, ROOK, KING, QUEEN
+    }
+    enum LocationX  {
+        A, B, C, D, E, F, G, H
+    }
  public static void main(String[] args){
    Scanner input = new Scanner(System.in);
-   System.out.println("Available Chess Pieces: \n1.Pawn \n2.Kight \n3.Bishop \n4.Rook \n5.King \n6.Queen");
-   System.out.print("Select a pice: ");
-   int pieceChoice = input.nextInt();
-   //TO-DO ERROR HANDELING FOR INCORRECT PICE + I THINKS ITS SUPOSED TO BE A CHAR
-   System.out.println("Please Select a Color: 1.Black 2.White");
-   int pieceColor = input.nextInt();
-   System.out.println("Please Enter a column: (a-h)");
-   char pieceColumn = input.next().charAt(0);
-   System.out.print("Please Enter a row: (1-8)");
-   int pieceRow = input.nextInt();
-   System.out.println(pieceColor);
 
+   //User input collection and error handling for piece type selection
+   PieceType piece; //Create variable to hold enum value for piece type
+   while (true){ //Loop to keep asking for user input in case user enters invalid piece type
+    System.out.println("Available Chess Pieces: \n1.Pawn \n2.Knight \n3.Bishop \n4.Rook \n5.King \n6.Queen");
+    System.out.print("Select a piece by typing its name: ");
+    String userInput = input.nextLine().toUpperCase(); //Turns user answer into uppercase to match enum values
+
+    try{ //Try catch block to catch invalid piece type input
+    PieceType pieceType = PieceType.valueOf(userInput.toUpperCase());
+    System.out.println("You selected: " + pieceType);
+    break; //Breaks out of the loop if user input is valid
+      }
+   catch(IllegalArgumentException e){
+    System.out.println("Invalid piece type. Please select a valid piece.");
+    }
+   }
+
+   //User input collection and error handling for color selection
+   Color color; //Create variable to hold enum value for color
+   while (true){ //Loop to keep asking for user input in case user enters invalid color
+    System.out.println("Chose Colors: \n1.White \n2.Black");
+    System.out.print("Select a color by typing it: ");
+    String userInput = input.nextLine().toUpperCase(); //Turns user answer into uppercase to match enum values
+
+    try{ //Try catch block to catch invalid color input
+    Color pieceColor = Color.valueOf(userInput.toUpperCase());
+    System.out.println("You selected: " + pieceColor);
+    break; //Breaks out of the loop if user input is valid
+      }
+   catch(IllegalArgumentException e){
+    System.out.println("Invalid color. Please select a valid color.");
+    }
+   }
  }
 }
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # AOOP-Lab-2
-Lab 2 for AOOP
-09/14/2026
-Changed from VS code
+Update Log
+
+Created Classes for all the chess pieces alongside
+their required constructors and methods
+
+In main began collecting user input and error
+handeling using enum 
+
