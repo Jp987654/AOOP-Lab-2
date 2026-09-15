3e5e10cf8d5a89b8eba46e00a1f4d152b763bc6c
JP <jose1950rivera1950@gmail.com>
2026-09-14 22:31:40 -0600

    added to processing user input

--- Chessboard.java
+++ Chessboard.java
@@ -1,3 +1,16 @@
 public class Chessboard {
-    
+    final int MAX_ROW = 8;
+    final int MIN_ROW = 1;
+
+    public boolean withinChessboard(char column, int row){
+        if (column < 'a' || column > 'h') {
+            return false;
+        }
+        else if (row < MIN_ROW || row > MAX_ROW) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
 }
